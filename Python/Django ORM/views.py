041b2cbcbb6dae174e8803a647cf382b174a4eb9
Kevin Bellagamba <bellagamba.ke@gmail.com>
2021-09-14 08:59:08 -0400

    Added py file
    Using django orm pull data into html files from views.py

---
+++ Python/Django ORM/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from .models import League, Team, Player
+
+from . import team_maker
+
+def index(request):
+	context = {
+		'leagues': League.objects.all(),
+		'teams': Team.objects.all(),
+		'players': Player.objects.all(),
+		# "leagues": League.objects.filter(name__contains="Baseball"),
+        # "leagues": League.objects.filter(name__contains="Womens"),
+        # "leagues": League.objects.filter(name__contains="Hockey"),
+        # "leagues": League.objects.exclude(name__contains="Football"),
+        # "leagues": League.objects.filter(name__contains="Conference"),
+        # "leagues": League.objects.filter(name__contains="Atlantic"),
+        # "teams": Team.objects.filter(location__contains="Dallas"),
+        # "teams": Team.objects.filter(team_name__contains="Raptors"),
+        # "teams": Team.objects.filter(location__contains="City"),
+        # "teams": Team.objects.filter(team_name__startswith="T"),
+        # "teams": Team.objects.all(),
+        # "players": Player.objects.filter(last_name__contains="Cooper"),
+        # "players": Player.objects.filter(first_name__contains="Joshua"),
+        # "players": Player.objects.filter(last_name__contains="Cooper").exclude(first_name__contains="Joshua"),
+        # "players": Player.objects.filter(first_name="Alexander") | Player.objects.filter(first_name="Wyatt"),
+	}
+	return render(request, "leagues/index.html", context)
+
+
+
+
+def make_data(request):
+	team_maker.gen_leagues(10)
+	team_maker.gen_teams(50)
+	team_maker.gen_players(200)
+
+	return redirect("index")
