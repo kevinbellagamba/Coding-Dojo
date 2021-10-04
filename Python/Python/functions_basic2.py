974ee65e1ced37f84a92c76404ef5464c8b0aec5
Kevin Bellagamba <bellagamba.ke@gmail.com>
2021-10-04 14:50:54 -0400

    Added py files

--- Python/Python/functions_basic2.py
+++ Python/Python/functions_basic2.py
@@ -19,7 +19,6 @@ def nums(nums_list):
 print(nums([1,2]))
 
 
-
 # First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
 # Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
 def add(nums_list):
